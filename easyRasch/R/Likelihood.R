89e53b347423de995728e1cd0d2a9b1e9fba5c5d
hyunoooh <hyunjoo.oh@wustl.edu>
2017-03-27 17:05:27 -0500

    Draft_03
    1) Create Likelihood.R file

--- easyRasch/R/Likelihood.R
+++ easyRasch/R/Likelihood.R
@@ -1,6 +1,6 @@
-#' Probability of answering the question correctly
+#' Likelihood of respondent's ability, theta 
 #' 
-#' Calculate the probability of respondent's answer is correct
+#' Calculate the likelihood of respondent's ability to get the answer correct
 #' 
 #' @param raschObj An object of class Rasch
 #' @param theta A proposed value of theta
@@ -12,35 +12,35 @@
 #' \item \code{y} A numeric vector of whether the respondent's answers are right(1) or wrong(0)
 #' }
 #' @author Hyunjoo Oh: \email{hyunjoo.oh@wustl.edu}
-#' @seealso \code{\link{Likelihood}}
 #' @note If the answer is correct, y=0; if the answer is incorrect, y=1. 
-#' We should know all the difficulty parameters, a. 
+#' We should know all n of the difficulty parameters a. 
 #' We assume that there is no missing data.
+#' @seealso \code{\link{Probability}}
 #' @examples 
 #' a <- as.numeric(c((sample(seq(-3, 3), size = 10, replace = TRUE))))
 #' Harry <- new("Rasch", name = 'Harry', a = a, y = as.numeric(c(sample(c(0,1), size=10, replace = TRUE))))
 #' Ron <- new("Rasch", name = 'Ron', a = a, y = as.numeric(c(sample(c(0,1), size=10, replace = TRUE))))
 #' Hermione <- new("Rasch", name = "Hermione", a = a, y = as.numeric(c(sample(c(0,1), size=10, replace = TRUE))))
-#' Probability(Harry, 1)
-#' Probability(Ron, 0.5)
-#' Probability(Hermione, 3)
+#' Likelihood(Harry, 1)
+#' Likelihood(Ron, 0.5)
+#' Likelihood(Hermione, 3)
 #' 
-#' @aliases Probability,ANY-method
-#' @rdname Probability
+#' @aliases Likelihood,ANY-method
+#' @rdname Likelihood
 #' @export
-setGeneric(name = "Probability",
+setGeneric(name = "Likelihood",
            def = function(raschObj, theta, ...){
-           standardGeneric("Probability")})
+             standardGeneric("Likelihood")})
 
 #' @export
-setMethod(f = "Probability",
+setMethod(f = "Likelihood",
           definition = function(raschObj, theta, ...){
-                            Prob.table <- showRasch(raschObj)
-                            P <- exp(theta - raschObj@a)/(1+exp(theta - raschObj@a))
-                            Q <- 1-P
-                            PQ <- ifelse((raschObj@y==1), P, Q)
-                            Prob.table <- rbind(Prob.table, P, PQ)
-                            row.names(Prob.table) <- c("a", "y", "P", "P or Q")
-                            print(Prob.table)
-                            }
-          )
+            Likeli.table <- showRasch(raschObj)
+            P <- exp(theta - raschObj@a)/(1+exp(theta - raschObj@a))
+            Q <- 1-P
+            Likeli.table <- rbind(Likeli.table, P, Q)
+            row.names(Likeli.table) <- c("a", "y", "P", "Q")
+            L <- t(Likeli.table[3,])%*%Likeli.table[4,]
+            print(L)
+          }
+)
